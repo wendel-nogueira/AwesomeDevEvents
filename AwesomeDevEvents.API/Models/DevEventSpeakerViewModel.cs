d2ec84b1740ba495fcd7b66e41a9cdd15eda29a2
Wendel Nogueira <43501645+wendel-nogueira@users.noreply.github.com>
2023-09-04 15:51:06 -0300

    FEAT: Add DTO mapper

---
+++ AwesomeDevEvents.API/Models/DevEventSpeakerViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AwesomeDevEvents.API.Models
+{
+    public class DevEventSpeakerViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string TalkTitle { get; set; }
+        public string TalkDescription { get; set; }
+        public string LinkedinProfile { get; set; }
+    }
+}
